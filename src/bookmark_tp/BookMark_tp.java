b63f0e65e66a8fd0e1df2d814eab49631db3d144
Ardillon-McBytes <jeanalains@gmail.com>
2018-03-17 12:36:12 -0400

    bug

--- src/bookmark_tp/BookMark_tp.java
+++ src/bookmark_tp/BookMark_tp.java
@@ -1,27 +1,3 @@
-<<<<<<< HEAD
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package bookmark_tp;
-
-/**
- *
- * @author moi
- */
-public class BookMark_tp {
-
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        // TODO code application logic here
-        // commit test old-dev
-    }
-    
-}
-=======
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
@@ -34,6 +10,8 @@ import static javafx.application.Application.launch;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+
+
 import javafx.stage.Stage;
 
 /**
@@ -41,46 +19,39 @@ import javafx.stage.Stage;
  * @author moi
  */
 public class BookMark_tp extends Application {
-    
-     
-    
-    
-      private Stage primaryStage;
-     
-        public void setPrimaryStage(Stage primaryStage){
-                   this.primaryStage = primaryStage;
-         }
-        
-        public Stage getPrimaryStage(){
-                  return this.primaryStage;
-         }
-    @Override
-    
-    
-    public void start(Stage stage) throws Exception {
-      
 
-   Parent root = FXMLLoader.load(getClass().getResource("Connexion_v1.fxml"));
+    private Stage primaryStage;
+
+    public void setPrimaryStage(Stage primaryStage) {
+        this.primaryStage = primaryStage;
+    }
+
+    public Stage getPrimaryStage() {
+        return this.primaryStage;
+    }
+
+    @Override
+
+    public void start(Stage stage) throws Exception {
+
+        Parent root = FXMLLoader.load(getClass().getResource("Connexion_v1.fxml"));
         Connexion_v1Controller controll = new Connexion_v1Controller();
         controll.setPrevStage(stage);
         Scene scene = new Scene(root);
-         Stage secondStage = new Stage();
+        Stage secondStage = new Stage();
         secondStage.setScene(scene);
         secondStage.show();
-    
-   
+
     }
 
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args)throws Exception {
-       
-  
+    public static void main(String[] args) throws Exception {
+
         SimpleDataSource.init("database.properties");
         launch(args);
-      
+
     }
-    
+
 }
->>>>>>> J-a
